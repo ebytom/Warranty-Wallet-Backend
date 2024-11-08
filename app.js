d442daef145e83c7af4cb510a8bdd31c3fa54703
Eby Tom <ebytomy7@gmail.com>
2024-11-09 00:55:34 +0530

    cors removed

--- app.js
+++ app.js
@@ -21,11 +21,7 @@ const warrantyRouter = require('./routes/warranty');
 const app = express();
 
 // middlewares
-app.use(cors({
-    origin: ['http://localhost:3000', 'http://localhost:3001'],
-    methods: ['GET', 'PUT', 'POST', 'DELETE', 'OPTIONS'],
-    maxAge: 10000,
-}))
+app.use(cors())
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
